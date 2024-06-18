d01f4f08cbca34e1b65869196359ff794b7480ae
Joshy14 <97353253+Joshy14@users.noreply.github.com>
2024-06-18 01:42:53 +0000

    Made the window bigger and implemented more MUI

--- NetShare/src/App.jsx
+++ NetShare/src/App.jsx
@@ -62,34 +62,34 @@ useWebSocket(WS_URL, {
         <img src={search}></img>
         <input className='searchFriends' type='search' placeholder='Search your friends'></input>
       </div>
-      <div className='soccerMom'>
-      <div className='DMreceiver'>
-        <p>Chat with</p>
-        <h2>Hansel Patel</h2>
-      </div>
-      <Stack 
-      sx={{overflowY:"scroll", scrollbarColor:"#fff"}}
-      width={"300px"} 
-      height={"490px"}>
-        <InboundMessage time="10:12am" message="Hey Sina, just making sure, you would like Sina, not Jared."></InboundMessage>
-        <OutboundMessage time="12:58pm" message="Yes, it is  Sina. Sorry for the confusion."></OutboundMessage>
-        <InboundMessage time="2:35am" message="Well, well, well. Sina, How- how do I explain this. I have to leave the country and go far away IMEDIATLY! Your promoted to the CEO. "></InboundMessage>
-        <OutboundMessage time="9:21am" message="I'm sorry, are you okay? Please just confirm with me that this message is fake."></OutboundMessage>
-        <OutboundMessage time="11:12am" message="Hello? I'm starting to worry aout you and our companys safety. Please respond."></OutboundMessage>
-        <OutboundMessage time="1:46pm" message="Wow. I just checked Pike13 and it appears my status is ''CEO''. I realy need you to tell me whats happening."></OutboundMessage>
-        <div className='messageInputDiv'>
-        <img className='pfp'src={sina}></img>
-        <input className='messageInput' type='search' placeholder='Type your message here' value = {message} onChange={(e)=>{
-        setMessage(e.target.value)
-        }}></input>
-       {/* <button className='sendButton' onClick={()=>{
-          send()
-        }}>Send</button> */}
-        <Button endIcon={<SendIcon />} size={"small"}sx={{margin:2, backgroundColor:"#fff", borderRadius:"10px"}}>send</Button>
+      <Stack width={"33%"}>
+        <div className='DMreceiver'>
+          <p>Chat with</p>
+          <h2>Hansel Patel</h2>
         </div>
+        <Stack 
+        sx={{overflowY:"scroll", scrollbarColor:"#fff"}}
+        width={"100%"} 
+        height={"85%"}>
+          <InboundMessage time="10:12am" message="Hey Sina, just making sure, you would like Sina, not Jared."></InboundMessage>
+          <OutboundMessage time="12:58pm" message="Yes, it is  Sina. Sorry for the confusion."></OutboundMessage>
+          <InboundMessage time="2:35am" message="Well, well, well. Sina, How- how do I explain this. I have to leave the country and go far away IMEDIATLY! Your promoted to the CEO. "></InboundMessage>
+          <OutboundMessage time="9:21am" message="I'm sorry, are you okay? Please just confirm with me that this message is fake."></OutboundMessage>
+          <OutboundMessage time="11:12am" message="Hello? I'm starting to worry aout you and our companys safety. Please respond."></OutboundMessage>
+          <OutboundMessage time="1:46pm" message="Wow. I just checked Pike13 and it appears my status is ''CEO''. I realy need you to tell me whats happening."></OutboundMessage>
+          <div className='messageInputDiv'>
+            <img className='pfp'src={sina}></img>
+            <input className='messageInput' type='search' placeholder='Type your message here' value = {message} onChange={(e)=>{
+            setMessage(e.target.value)
+            }}></input>
+          {/* <button className='sendButton' onClick={()=>{
+              send()
+            }}>Send</button> */}
+            <Button endIcon={<SendIcon />} size={"small"}sx={{margin:2, backgroundColor:"#fff", borderRadius:"10px"}}>send</Button>
+          </div>
       </Stack>
     
-    </div>
+    </Stack>
       
     </div>
   )
